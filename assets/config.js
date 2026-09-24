a693bb7df86b8d876761d35023b78f37e0a10ac5
Thanistan <thanistantssm@gmail.com>
2026-09-24 12:09:54 +0530

    Connect public dashboard to deployed API

--- assets/config.js
+++ assets/config.js
@@ -1,1 +1,1 @@
-window.X10_CONFIG = Object.assign({ API_BASE: '/api' }, window.X10_CONFIG || {});
+window.X10_CONFIG = Object.assign({ API_BASE: 'https://apix10think.cloudvideo.online/api' }, window.X10_CONFIG || {});
